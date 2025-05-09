2ff0616390af99e1d12014350558b35fd5fdd628
jmg008 <jimingyeom@gmail.com>
2025-05-09 16:46:16 +0900

    FEAT: 영상 전달받은 후 gemini-2.5-flash-preview-04-17에 전달해 조언 출력

--- app/api/test/route.tsx
+++ app/api/test/route.tsx
@@ -1,8 +1,27 @@
 import { NextResponse } from "next/server";
+import { GoogleGenAI, } from "@google/genai";
 
-export const GET = async function GetHandler() {
+ 
+ export const POST = async function PostHandler(request:Request) {
+  const ai = new GoogleGenAI({ apiKey: "YAIzaSyCpYIW4xP3f2yNzzd8lACYuJBIssLYnbkE" });
+  const req = await request.json();
+  const blob = req.video
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "video/mov",
+        data: blob,
+      },
+    },
+    { text: "You are helpful balling advice AI assistant. After watching this balling video, give appropriate advice for improvement. You must use a friendly tone. You must response in Korean." }
+  ];
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-preview-04-17",
+    contents: contents,
+  });
   return NextResponse.json({
     success: false,
-    message: '사용자 정보를 찾을 수 없어요'
+    message: response.text
   }, { status: 404 });
 }
