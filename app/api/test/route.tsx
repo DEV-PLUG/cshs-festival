6a480df66aca587c790d4fa8325724d97d93da79
jmg008 <jimingyeom@gmail.com>
2025-05-12 17:42:23 +0900

    UPDATE: 여러모로 안되고있어요

--- app/api/test/route.tsx
+++ app/api/test/route.tsx
@@ -1,26 +1,49 @@
 import { NextResponse } from "next/server";
-import { GoogleGenAI, } from "@google/genai";
-
+import {
+  GoogleGenAI,
+  createUserContent,
+  createPartFromBase64
+ } from "@google/genai";
+import fs from "fs"
  
 export const POST = async function PostHandler(request:Request) {
   const ai = new GoogleGenAI({ apiKey: "AIzaSyBNtBw7IYI4Gi4b3nF4AEx8vJBcQkKJLfc" });
-  const req = await request.json();
-  const { video, type } = req;
+  // const req = await request.json();
+  // const { video, type } = req;
 
+  let video
+  fs.readFile("public/WIN_20250512_16_01_57_Pro.mp4", (err, data) => {
+    if (err) {
+      console.log("----------")
+      console.log(err)
+      console.log("----------")
+      return
+    }
+    video = typeof(data)
+  })
+  const type = "video/mp4"
+  console.log("?"+video+"?")
+  // const contents = createUserContent([
+  //   "You are helpful balling advice AI assistant. After watching this balling video, give appropriate advice for improvement. You must use a friendly tone. You must response in Korean.",
+  //   createPartFromBase64(video, type)
+  // ])
   const contents = [
     {
       inlineData: {
         mimeType: type,
-        data: video,
+        data: video
       },
-    },
-    { text: "You are helpful balling advice AI assistant. After watching this balling video, give appropriate advice for improvement. You must use a friendly tone. You must response in Korean." }
-  ];
-  
+    }
+  ]
+  console.log(contents)
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
     contents: contents,
+    config: {
+      systemInstruction: "You are helpful balling advice AI assistant. After watching this balling video, give appropriate advice for improvement. You must use a friendly tone. You must response in Korean."
+    }
   });
+  console.log(response.text)
   return NextResponse.json({
     success: false,
     message: response.text
